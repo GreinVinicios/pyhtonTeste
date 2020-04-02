d2cea1ff5cdd9d45f171c75d03b569503467dc40
Vinícios Grein <vinicios.grein@gmail.com>
2020-04-02 14:05:17 -0300

    Aula 1

---
+++ advinhacao.py
@@ -0,0 +1,16 @@
+print("***********")
+print("Bem Vindo")
+print("***********")
+
+numero_secreto = 43
+
+chute = input("Digite o seu nº: ")
+
+print("Você digitou: ", chute)
+
+if (numero_secreto == int(chute)) :
+    print("Acertou")
+else:
+    print("Errou")
+
+print("Fim de jogo!")
